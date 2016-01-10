cc6707ea8bb03ecccf5f512fc9fb2b502b999f71
Justin Young <justinyoung@Justins-MacBook-Pro.local>
2016-01-10 00:02:32 -0800

    Added map and diagonal section breaks

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var windowHeight = '';
+var jsHeroTarget = '';
+
+$(document).ready(function() {
+  
+
+
+
+});
+
+$(document).ready(heroResize);
+$(window).resize(heroResize);
+
+function heroResize() {
+	// Make the element the height of the window
+
+	windowHeight = $(window).height();
+	jsHeroTarget = $('.js--hero-resize');
+	jsHeroTarget.css('min-height', windowHeight + 200);
+	
+}
