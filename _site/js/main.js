dc1888e137205f83155007c544f7ca798d13f01e
Justin Young <justin.barr.young@gmail.com>
2016-05-22 21:00:28 -0700

    Got menu working. It scrolls.

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,15 +1,6 @@
 var windowHeight = '';
 var jsHeroTarget = '';
-
-$(document).ready(function() {
-
-
-
-
-});
-
-$(document).ready(heroResize);
-$(window).resize(heroResize);
+var body = $('html, body');
 
 function heroResize() {
 	// Make the element the height of the window
@@ -19,3 +10,45 @@ function heroResize() {
 	jsHeroTarget.css('height', windowHeight);
 
 }
+
+$(document).ready(heroResize);
+$(window).resize(heroResize);
+
+
+
+$(document).ready(function() {
+
+var details_link = $("#details-link");
+var story_link = $("#story-link");
+var ceremony_link = $("#ceremony-link");
+var travel_link = $("#travel-link");
+
+details_link.click(function(){
+	body.animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 500);
+  return false;
+});
+
+story_link.click(function(){
+	body.animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 500);
+  return false;
+});
+
+ceremony_link.click(function(){
+	body.animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 500);
+  return false;
+});
+
+travel_link.click(function(){
+	body.animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 500);
+  return false;
+});
+
+});
